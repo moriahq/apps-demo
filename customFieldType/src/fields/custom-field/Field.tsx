9640c241d962d9de32a194e090889c236a951deb
宋启炎 <songqiyan@oschina.cn>
2025-07-23 15:46:24 +0800

    update

--- customFieldType/src/fields/custom-field/Field.tsx
+++ customFieldType/src/fields/custom-field/Field.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Input } from "antd";
 
 const Field: React.FC<Record<string, any>> = (props) => {
   const { value, onChange, itemId } = props;
 
-  return <div>custom field</div>;
+  return <Input value={value} onChange={onChange} style={{ width: "100%" }} />;
 };
 
 export default Field;
